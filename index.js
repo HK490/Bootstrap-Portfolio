40a615ad8e962f7b7eb45de201c21baab3c08533
Helen Kim <Helenk490@gmail.com>
2019-09-09 23:25:45 -0400

    changed styling

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
 
+
+    
+
     $("#home").click(function () {
+        $( "#first").hide();
+        $("#first").fadeIn(2000);
         $('html,body').animate({
             scrollTop: $("#first").offset().top
         });
     });
 
     $("#port").click(function () {
+        $( "#second").hide();
+        $("#second").fadeIn(2000);
         $('html,body').animate({
             scrollTop: $("#second").offset().top
         });
+        
     });
 
     $("#cont").click(function () {
+        $( "#third").hide();
+        $("#third").fadeIn(2000);
         $('html,body').animate({
             scrollTop: $("#third").offset().top
         });
