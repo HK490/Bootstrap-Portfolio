0a275d414ce82b8022f4deb87bb0e2dcf8b26217
Helen Kim <Helenk490@gmail.com>
2020-04-23 20:07:48 -0400

    changed banner and nav

--- index.js
+++ index.js
@@ -28,4 +28,15 @@ $(document).ready(function() {
   // if ($(document).scrollTop() + $(document).height() >= $(document).height()) {
   //     $("body").append($(".container"))
   // }
+
+  $(document).scroll(function() {
+    if ($(document).scrollTop() >= 600) {
+      // user scrolled 50 pixels or more;
+      // do stuff
+      $(".nav").css({"background-color": "#f9f9f9", "box-shadow": "inset 7px 7px 10px 10px white", "box-shadow": "3px 3px 6px 6px white"})
+    } else {
+      $(".nav").css({"background-color": "transparent", "box-shadow": "none"})
+    }
+  });
+
 });
